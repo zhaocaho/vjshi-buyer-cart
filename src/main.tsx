943b903d2d2418de350ae50411b45bb30f1452ac
henry <243211921@qq.com>
2025-04-09 16:37:41 +0800

    feat: 修改checkbox的lineWidth

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from "./App.tsx";
 import { worker } from "../mocks/browser.ts";
 import { store } from "./store/index.ts";
 import { Provider } from "react-redux";
+
 import "@ant-design/v5-patch-for-react-19";
+import { ConfigProvider } from "antd";
 
 //TODO 为了方便项目掩饰，mock接口这里未区分环境。
 worker
@@ -14,7 +16,17 @@ worker
     createRoot(document.getElementById("root")!).render(
       <StrictMode>
         <Provider store={store}>
-          <App />
+          <ConfigProvider
+            theme={{
+              components: {
+                Checkbox: {
+                  lineWidth: 2, // 选框宽度
+                },
+              },
+            }}
+          >
+            <App />
+          </ConfigProvider>
         </Provider>
       </StrictMode>,
     );
